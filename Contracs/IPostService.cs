6aa5cd1e1992b43036a70fdefd15871332c8a95c
vanchi3 <ivanamircheva73@gmail.com>
2024-03-14 08:53:47 +0200

    Add post service

---
+++ Contracs/IPostService.cs
@@ -0,0 +1,11 @@
+﻿using OmniTalks.Models;
+
+namespace OmniTalks.Contracs
+{
+    public interface IPostService
+    {
+        public Task Add(PostViewModel model,string currentId);
+        public Task<List<PostViewModel>> All();
+
+    }
+}
---
+++ Services/PostService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OmniTalks.Contracs;
+using OmniTalks.Data;
+using OmniTalks.Models;
+using OmniTalks.Models.Domein;
+using System.Security.Claims;
+
+
+namespace OmniTalks.Services
+{
+    public class PostService:IPostService
+    {
+        public ApplicationDbContext _context;
+
+        public PostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(PostViewModel model,string currentId)
+        {
+            Post post = new Post()
+            {
+                Id = Guid.NewGuid(),
+                Content = model.Content,
+                UserId = Guid.Parse(currentId)
+            };
+
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<PostViewModel>> All()
+        {
+            var posts = await _context.Posts.ToListAsync();
+
+            var  postViewModels = posts.Select(x => new PostViewModel()
+            {
+                Content = x.Content,
+                UserId = x.UserId,
+                User = x.User,
+                PostLikes = x.PostLikes,
+                Comments = x.Comments
+
+            })
+            .ToList();
+
+            return postViewModels;
+        }
+      
+    }
+}
+
